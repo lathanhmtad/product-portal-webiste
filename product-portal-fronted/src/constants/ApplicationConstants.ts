810606b8d5d9e5d4f44d32f84c7ef998d80efb0a
lathanhmtad <d0763705638@gmail.com>
2024-06-01 11:30:58 +0700

    final exam

--- product-portal-fronted/src/constants/ApplicationConstants.ts
+++ product-portal-fronted/src/constants/ApplicationConstants.ts
@@ -1,5 +1,6 @@
 class ApplicationConstants {
-    static API_PATH = 'https://website-portal-backend.onrender.com/api';
+    // static API_PATH = 'https://website-portal-backend.onrender.com/api';
+    static API_PATH = 'http://localhost:8080/api';
 }
 
 export default ApplicationConstants;
